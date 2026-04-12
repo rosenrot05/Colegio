ac7e9e921ebc311ca0b839c47ce5916d87b27411
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-04-11 23:26:38 -0500

    creacion del proyecto y de paquetes (controlador, modelo, vista)

---
+++ src/main/java/vista/SistemaColegio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vista;
+
+/**
+ *
+ * @author rosam
+ */
+public class SistemaColegio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
